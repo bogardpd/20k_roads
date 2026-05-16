9ce4c418464e678d5e57b90dc1a0eaca27038e09
Paul Bogard <paulbogard@gmail.com>
2026-05-16 08:52:32 -0400

    Add --tracks argument

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # 20k_roads
-Tool to compare GPS driving tracks to OSM roads
+
+A tool to compare GPS driving tracks to OSM roads.
+
+## Usage
+
+```
+python roads.py --osm region.osm.pbf --tracks tracks.gpkg
+```
+
+### Arguments
+
+- **`--osm`:** An OpenStreetMap PBF file containing roads for the region(s) that all of the driving tracks are in.
+- **`--tracks`:** A GeoPackage file that contains a `driving_tracks` MultiLineString layer.
+
+## Notes
+
+OpenStreetMap PBF extracts can be downloaded from [Geofabrik downloads](https://download.geofabrik.de/). Once they're downloaded, use [Osmium Tool](https://osmcode.org/osmium-tool/) to filter them to only roads with the following command:
+
+```
+osmium tags-filter region-latest.osm.pbf w/highway -o region-roads.osm.pbf
+```
--- roads.py
+++ roads.py
@@ -3,7 +3,8 @@ import geopandas as gpd
 import osmium
 from pathlib import Path
 
-def find_roads(osm_data):
+def find_roads(osm_data, track_file):
+    print("Loading OSM roads...")
     fp = (
         osmium.FileProcessor(osm_data)
         .with_locations()
@@ -15,12 +16,29 @@ def find_roads(osm_data):
     )
     roads = gpd.GeoDataFrame.from_features(fp, crs='EPSG:4326')
     print(roads)
+    print("Loading tracks...")
+    tracks = gpd.read_file(track_file,
+        layer='driving_tracks',
+        fid_as_index=True,
+        columns=['utc_start'],
+    )
+    tracks = tracks.sort_values('utc_start')
+    print(tracks)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="find_roads",
         description="Matches GPS tracks to roads",
     )
-    parser.add_argument("--osm", type=Path, required=True)
+    parser.add_argument("--osm",
+        type=Path,
+        required=True,
+        help="OpenStreetMap PBF file covering the region of the tracks",
+    )
+    parser.add_argument("--tracks",
+        type=Path,
+        required=True,
+        help="GeoPackage file containing driving tracks",
+    )
     args = parser.parse_args()
-    find_roads(args.osm)
+    find_roads(args.osm, args.tracks)
