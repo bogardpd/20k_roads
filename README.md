27aad504155bfb8111bdc3f9f69ddc84ed2f231b
Paul Bogard <paulbogard@gmail.com>
2026-05-16 10:27:34 -0400

    Add --states argument

--- README.md
+++ README.md
@@ -5,15 +5,18 @@ A tool to compare GPS driving tracks to OSM roads.
 ## Usage
 
 ```
-python roads.py --osm region.osm.pbf --tracks tracks.gpkg
+python roads.py --osm region.osm.pbf --states tl_2023_us_state.zip --tracks tracks.gpkg
 ```
 
 ### Arguments
 
 - **`--osm`:** An OpenStreetMap PBF file containing roads for the region(s) that all of the driving tracks are in.
+- **`--states`:** Census TIGER data for U.S. state outlines, used to disambiguate state routes with the same number in different states.
 - **`--tracks`:** A GeoPackage file that contains a `driving_tracks` MultiLineString layer with a `utc_start` datetime column.
 
-## Notes
+## Data Sources
+
+Census TIGER state data can be found here: [https://www2.census.gov/geo/tiger/TIGER2023/STATE/tl_2023_us_state.zip](https://www2.census.gov/geo/tiger/TIGER2023/STATE/tl_2023_us_state.zip)
 
 OpenStreetMap PBF extracts can be downloaded from [Geofabrik downloads](https://download.geofabrik.de/). Once they're downloaded, use [Osmium Tool](https://osmcode.org/osmium-tool/) to filter them to only drivable roads with the following command:
 
--- roads.py
+++ roads.py
@@ -3,7 +3,16 @@ import geopandas as gpd
 import osmium
 from pathlib import Path
 
-def find_roads(osm_data, track_file):
+METRIC_CRS = 'EPSG:5070' # CONUS Albers Metric
+
+def find_roads(osm_data, state_data, track_file):
+    print("Loading states...")
+    states = gpd.read_file(state_data)
+    states = states[['STUSPS', 'NAME', 'geometry']]rename(columns={
+        'STUSPS': 'state_abbr',
+        'NAME': 'state_name',
+    })
+
     print("Loading OSM roads...")
     fp = (
         osmium.FileProcessor(osm_data)
@@ -11,11 +20,14 @@ def find_roads(osm_data, track_file):
         .with_filter(osmium.filter.EntityFilter(osmium.osm.WAY))
         .with_filter(osmium.filter.KeyFilter('highway'))
         .with_filter(osmium.filter.GeoInterfaceFilter(tags=[
-            'highway', 'name', 'ref'
+            'highway', 'name', 'ref', 'network', 'state'
         ]))
     )
     roads = gpd.GeoDataFrame.from_features(fp, crs='EPSG:4326')
+    # Spatially join onto state boundaries.
+    roads = gpd.sjoin(roads, states, how='left', predicate='within')
     print(roads)
+
     print("Loading tracks...")
     tracks = gpd.read_file(track_file,
         layer='driving_tracks',
@@ -23,7 +35,10 @@ def find_roads(osm_data, track_file):
         columns=['utc_start'],
     )
     tracks = tracks.sort_values('utc_start')
-    print(tracks)
+
+    print("Reprojecting to metric CRS...")
+    roads_proj = roads.to_crs(METRIC_CRS)
+    tracks_proj = tracks.to_crs(METRIC_CRS)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -35,10 +50,15 @@ if __name__ == "__main__":
         required=True,
         help="OpenStreetMap PBF file covering the region of the tracks",
     )
+    parser.add_argument("--states",
+        type=Path,
+        required=True,
+        help="Census TIGER data for U.S. state boundaries",
+    )
     parser.add_argument("--tracks",
         type=Path,
         required=True,
         help="GeoPackage file containing driving tracks",
     )
     args = parser.parse_args()
-    find_roads(args.osm, args.tracks)
+    find_roads(args.osm, args.states, args.tracks)
