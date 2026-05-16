eda2f8332fc5ebdc5f913b2e14ede73305b874cd
Paul Bogard <paulbogard@gmail.com>
2026-05-16 12:09:18 -0400

    Create build_roads and build_tracks functions

--- roads.py
+++ roads.py
@@ -6,15 +6,26 @@ from pathlib import Path
 OSM_CRS = 'EPSG:4326'
 METRIC_CRS = 'EPSG:5070' # CONUS Albers Metric
 
-def find_roads(osm_data, state_data, track_file):
-    print("Loading states...")
+def find_roads(osm_data: Path, state_data: Path, track_file: Path) -> None:
+    """Matches tracks to unique OSM roads."""
+
+    print("Loading OSM roads...", end=" ")
+    roads = build_roads(osm_data, state_data)
+    print("done.")
+    print(roads)
+    
+    print("Loading tracks...", end=" ")
+    tracks = build_tracks(track_file)
+    print("done.")
+    
+
+def build_roads(osm_data: Path, state_data: Path) -> gpd.GeoDataFrame:
+    """Creates a road GeoDataFrame with state labels."""
     states = gpd.read_file(state_data)
     states = states[['STUSPS', 'NAME', 'geometry']].rename(columns={
         'STUSPS': 'state_abbr',
         'NAME': 'state_name',
     }).to_crs(OSM_CRS)
-
-    print("Loading OSM roads...")
     fp = (
         osmium.FileProcessor(osm_data)
         .with_locations()
@@ -25,21 +36,20 @@ def find_roads(osm_data, state_data, track_file):
         ]))
     )
     roads = gpd.GeoDataFrame.from_features(fp, crs=OSM_CRS)
-    # Spatially join onto state boundaries.
+    # Spatially join U.S. states onto roads.
     roads = gpd.sjoin(roads, states, how='left', predicate='within')
-    print(roads)
+    return roads.to_crs(METRIC_CRS)
 
-    print("Loading tracks...")
-    tracks = gpd.read_file(track_file,
+def build_tracks(track_file: Path) -> gpd.GeoDataFrame:
+    tracks = gpd.read_file(
+        track_file,
         layer='driving_tracks',
         fid_as_index=True,
         columns=['utc_start'],
     )
     tracks = tracks.sort_values('utc_start')
+    return tracks.to_crs(METRIC_CRS)
 
-    print("Reprojecting to metric CRS...")
-    roads_proj = roads.to_crs(METRIC_CRS)
-    tracks_proj = tracks.to_crs(METRIC_CRS)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
