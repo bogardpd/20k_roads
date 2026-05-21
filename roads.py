591c8e0af0fad584e772963ef3f4526c71e8ba88
Paul Bogard <paulbogard@gmail.com>
2026-05-20 17:56:18 -0700

    Write OSM PBF checksum to file

--- roads.py
+++ roads.py
@@ -1,10 +1,13 @@
 import argparse
 import geopandas as gpd
+import hashlib
+import json
 import pandas as pd
 import osmium
 import re
 import tomllib
 from collections import defaultdict
+from datetime import datetime, timezone
 from pathlib import Path
 from shapely.geometry import Point, LineString, MultiLineString
 from shapely.wkb import loads as wkb_loads
@@ -14,22 +17,38 @@ with open('config.toml', 'rb') as f:
 
 class OSMDataContainer():
     """Holds OSM data."""
-    def __init__(self, osm_data, state_data):
-        self.osm_data_dir: Path | None = None
+    def __init__(self, osm_data_path, state_data_path):
+        self.osm_data_path: Path = osm_data_path
+        self.state_data_path: Path = state_data_path
+        self.osm_checksum = self._osm_checksum()
         self.ways: gpd.GeoDataFrame | None = None
         self.ways_sindex = None
         self.node_ways: dict | None = None
-        self.load_osm(osm_data, state_data)
+        self.load_osm()
 
-    def load_osm(self, osm_data, state_data):
+    def load_osm(self):
         """Loads OSM data."""
-        self.osm_data_dir = osm_data.parent
         # TODO: check for pickle before processing data.
-        handler = RoadHandler(state_data)
-        handler.apply_file(osm_data, locations=True)
+        handler = RoadHandler(self.state_data_path)
+        handler.apply_file(self.osm_data_path, locations=True)
+        with open(self.osm_data_path.with_suffix('.checksum.json'), 'w') as f:
+            # Store checksum of OSM PBF file.
+            metadata = {
+                'source': str(self.osm_data_path),
+                'checksum': self.osm_checksum,
+                'processed_at': datetime.now(timezone.utc).isoformat(),
+            }
+            json.dump(metadata, f, indent=2)
         self.ways = handler.ways
         self.node_ways = handler.node_ways
         self.ways_sindex = self.ways.sindex # Build spatial index
+
+    def _osm_checksum(self):
+        h = hashlib.sha256()
+        with open(self.osm_data_path, 'rb') as f:
+            while chunk := f.read(1 << 20):
+                h.update(chunk)
+        return h.hexdigest()
         
 
 class RoadHandler(osmium.SimpleHandler):
